4fa9a3376256d901afdcf04c69ba1b6137405447
Sachchithananthan Gowreeshan <gowreeshan1@gmail.com>
2023-10-24 15:36:59 +0530

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -1,59 +1,27 @@
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""A library to evaluate Inception on a single GPU.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import os
 from PIL import Image
 
-from inception.slim import slim
-import numpy as np
-import tensorflow as tf
-from tensorflow.compat.v1 import flags
+# Define your flags using tf.compat.v1.flags
+FLAGS = tf.compat.v1.flags.FLAGS
 
-import math
-import os.path
-import scipy.misc
-# import time
-# import scipy.io as sio
-# from datetime import datetime
-import sys
-if sys.version_info[0] == 2:
-    import cPickle as pickle
-else:
-    import pickle
+tf.compat.v1.flags.DEFINE_string('checkpoint_dir',
+                               './inception_finetuned_models/birds_valid299/model.ckpt-5000',
+                               """Path where to read model checkpoints.""")
 
+tf.compat.v1.flags.DEFINE_string('image_folder',
+                               '/Users/han/Documents/CUB_200_2011/CUB_200_2011/images',
+                               """Path where to load the images """)
 
-FLAGS = flags.FLAGS
+tf.compat.v1.flags.DEFINE_integer('num_classes', 50,
+                              """Number of classes """)
+tf.compat.v1.flags.DEFINE_integer('splits', 10,
+                              """Number of splits """)
+tf.compat.v1.flags.DEFINE_integer('batch_size', 64, "batch size")
+tf.compat.v1.flags.DEFINE_integer('gpu', 1, "The ID of GPU to use")
 
-flags.DEFINE_string('checkpoint_dir',
-                           './inception_finetuned_models/birds_valid299/model.ckpt-5000',
-                           """Path where to read model checkpoints.""")
-
-flags.DEFINE_string('image_folder', 
-							'/Users/han/Documents/CUB_200_2011/CUB_200_2011/images',
-							"""Path where to load the images """)
-
-flags.DEFINE_integer('num_classes', 50,      # 20 for flowers
-                            """Number of classes """)
-flags.DEFINE_integer('splits', 10,
-                            """Number of splits """)
-flags.DEFINE_integer('batch_size', 64, "batch size")
-flags.DEFINE_integer('gpu', 1, "The ID of GPU to use")
 # Batch normalization. Constant governing the exponential moving average of
 # the 'global' mean and variance for all activations.
 BATCHNORM_MOVING_AVERAGE_DECAY = 0.9997
@@ -61,171 +29,120 @@ BATCHNORM_MOVING_AVERAGE_DECAY = 0.9997
 # The decay to use for the moving average.
 MOVING_AVERAGE_DECAY = 0.9999
 
-
 fullpath = FLAGS.image_folder
 print(fullpath)
 
 
 def preprocess(img):
-    # print('img', img.shape, img.max(), img.min())
-    # img = Image.fromarray(img, 'RGB')
     if len(img.shape) == 2:
         img = np.resize(img, (img.shape[0], img.shape[1], 3))
-    img = scipy.misc.imresize(img, (299, 299, 3),
-                              interp='bilinear')
+    img = np.array(Image.fromarray(img).resize((299, 299), resample=Image.BILINEAR))
     img = img.astype(np.float32)
-    # [0, 255] --> [0, 1] --> [-1, 1]
-    img = img / 127.5 - 1.
-    # print('img', img.shape, img.max(), img.min())
+    img = (img / 127.5) - 1.0
     return np.expand_dims(img, 0)
 
 
-def get_inception_score(sess, images, pred_op):
+def get_inception_score(images, pred_op):
     splits = FLAGS.splits
-    # assert(type(images) == list)
-    assert(type(images[0]) == np.ndarray)
-    assert(len(images[0].shape) == 3)
-    assert(np.max(images[0]) > 10)
-    assert(np.min(images[0]) >= 0.0)
+    assert (type(images[0]) == np.ndarray)
+    assert (len(images[0].shape) == 3)
+    assert (np.max(images[0]) > 10)
+    assert (np.min(images[0]) >= 0.0)
     bs = FLAGS.batch_size
     preds = []
     num_examples = len(images)
-    n_batches = int(math.floor(float(num_examples) / float(bs)))
+    n_batches = int(np.floor(float(num_examples) / float(bs))
     indices = list(np.arange(num_examples))
     np.random.shuffle(indices)
     for i in range(n_batches):
         inp = []
-        # print('i*bs', i*bs)
         for j in range(bs):
-            if (i*bs + j) == num_examples:
+            if (i * bs + j) == num_examples:
                 break
-            img = images[indices[i*bs + j]]
-            # print('*****', img.shape)
+            img = images[indices[i * bs + j]]
             img = preprocess(img)
             inp.append(img)
-        # print("%d of %d batches" % (i, n_batches))
-        # inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
         inp = np.concatenate(inp, 0)
-        #  print('inp', inp.shape)
-        pred = sess.run(pred_op, {'inputs:0': inp})
+        pred = pred_op(inp)
         preds.append(pred)
-        # if i % 100 == 0:
-        #     print('Batch ', i)
-        #     print('inp', inp.shape, inp.max(), inp.min())
     preds = np.concatenate(preds, 0)
     scores = []
     for i in range(splits):
         istart = i * preds.shape[0] // splits
         iend = (i + 1) * preds.shape[0] // splits
         part = preds[istart:iend, :]
-        kl = (part * (np.log(part) -
-              np.log(np.expand_dims(np.mean(part, 0), 0))))
+        kl = (part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0)))
+              )
         kl = np.mean(np.sum(kl, 1))
         scores.append(np.exp(kl))
-    print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores))
+    print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores)
+          )
     return np.mean(scores), np.std(scores)
 
 
 def load_data(fullpath):
-    print(fullpath)
     images = []
     for path, subdirs, files in os.walk(fullpath):
         for name in files:
-            if name.rfind('jpg') != -1 or name.rfind('png') != -1:
+            if name.endswith('jpg') or name.endswith('png'):
                 filename = os.path.join(path, name)
-                # print('filename', filename)
-                # print('path', path, '\nname', name)
-                # print('filename', filename)
                 if os.path.isfile(filename):
-                    img = scipy.misc.imread(filename)
+                    img = np.array(Image.open(filename))
                     images.append(img)
     print('images', len(images), images[0].shape)
     return images
 
 
-def inference(images, num_classes, for_training=False, restore_logits=True,
-              scope=None):
-    """Build Inception v3 model architecture.
-
-    See here for reference: http://arxiv.org/abs/1512.00567
-
-    Args:
-    images: Images returned from inputs() or distorted_inputs().
-    num_classes: number of classes
-    for_training: If set to `True`, build the inference model for training.
-      Kernels that operate differently for inference during training
-      e.g. dropout, are appropriately configured.
-    restore_logits: whether or not the logits layers should be restored.
-      Useful for fine-tuning a model with different num_classes.
-    scope: optional prefix string identifying the ImageNet tower.
-
-    Returns:
-    Logits. 2-D float Tensor.
-    Auxiliary Logits. 2-D float Tensor of side-head. Used for training only.
-    """
-    # Parameters for BatchNorm.
+def inference(images, num_classes, for_training=False, restore_logits=True, scope=None):
     batch_norm_params = {
-      # Decay for the moving averages.
-      'decay': BATCHNORM_MOVING_AVERAGE_DECAY,
-      # epsilon to prevent 0s in variance.
-      'epsilon': 0.001,
+        'decay': BATCHNORM_MOVING_AVERAGE_DECAY,
+        'epsilon': 0.001,
     }
-    # Set weight_decay for weights in Conv and FC layers.
-    with slim.arg_scope([slim.ops.conv2d, slim.ops.fc], weight_decay=0.00004):
-        with slim.arg_scope([slim.ops.conv2d],
-                            stddev=0.1,
-                            activation=tf.nn.relu,
-                            batch_norm_params=batch_norm_params):
-            logits, endpoints = slim.inception.inception_v3(
-              images,
-              dropout_keep_prob=0.8,
-              num_classes=num_classes,
-              is_training=for_training,
-              restore_logits=restore_logits,
-              scope=scope)
 
-    # Grab the logits associated with the side head. Employed during training.
-    auxiliary_logits = endpoints['aux_logits']
+    # Set weight_decay for weights in Conv and FC layers.
+    with tf.compat.v1.keras.layers.experimental.preprocessing():
+        with tf.compat.v1.keras.layers.Conv2D(kernel_regularizer=keras.regularizers.l2(0.00004)):
+            with tf.compat.v1.keras.layers.BatchNormalization():
+                logits, endpoints = keras.applications.InceptionV3(
+                    include_top=True,
+                    weights=None,
+                    input_tensor=images,
+                    input_shape=(299, 299, 3),
+                    classes=num_classes,
+                )
+
+    auxiliary_logits = endpoints['predictions']
 
     return logits, auxiliary_logits
 
 
 def main(unused_argv=None):
-    FLAGS = flags.FLAGS
-    FLAGS.num_classes = 50
+    # Evaluate the model on the dataset
 
-	# Build the inference model
-    model = tf.keras.applications.InceptionV3(weights='imagenet', include_top=False,input_shape = (299, 299, 3))
+    with tf.device("/GPU:" + str(FLAGS.gpu)):
+        # Number of classes in the Dataset label set plus 1.
+        # Label 0 is reserved for an (unused) background class.
+        num_classes = FLAGS.num_classes + 1
 
-	# Build a Graph that computes the logits predictions from the
-	# inference model.
-    inputs = tf.keras.Input(shape=[299, 299, 3], dtype=tf.float32, name='inputs')
-    logits, _ = model(inputs)
+        # Build a Graph that computes the logits predictions from the
+        # inference model.
+        inputs = tf.keras.layers.Input(shape=(299, 299, 3))
+        logits, _ = inference(inputs, num_classes)
 
-	# calculate softmax after remove 0 which reserve for BG
-    known_logits = tf.slice(logits, [0, 1], [-1, -1])
-    pred_op = tf.nn.softmax(known_logits)
+        # Calculate softmax after removing the class for BG
+        known_logits = tf.slice(logits, [0, 1], [FLAGS.batch_size, num_classes - 1])
+        pred_op = tf.nn.softmax(known_logits)
 
-	# Restore the moving average version of the
-	# learned variables for eval.
-    checkpoint_reader = tf.train.load_checkpoint(FLAGS.checkpoint_dir)
-    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY)
-    variables_to_restore = variable_averages.variables_to_restore()
-    restore_op_name_to_variable_name = checkpoint_reader.get_variable_to_shape_map()
-    restore_variable_map = {}
-    for key_name, variable in variables_to_restore.items():
-      restore_variable_name = restore_op_name_to_variable_name.get(key_name)
-      if restore_variable_name is None:
-    # skip variables in the optimizer graph
-        continue
-      print('restore %s from %s' % (key_name, restore_variable_name))
-      restore_variable_map[key_name] = tf.train.get_variable_by_name(restore_variable_name)
-    tf.train.Saver(restore_variable_map).restore(tf.keras.backend.get_session(), FLAGS.checkpoint_dir)
+        # Restore the model
+        variables_to_restore = tf.train.list_variables(FLAGS.checkpoint_dir)
+        variable_dict = {}
+        for name, shape in variables_to_restore:
+            variable_dict[name] = tf.Variable(initial_value=tf.train.load_variable(FLAGS.checkpoint_dir, name))
+        tf.train.Checkpoint(**variable_dict).restore(FLAGS.checkpoint_dir)
+        print('Restore the model from %s).' % FLAGS.checkpoint_dir)
 
-	# Evaluate the model on the Dataset
-    images = load_data(fullpath)
-    get_inception_score(images, pred_op)
-
+        images = load_data(fullpath)
+        get_inception_score(images, pred_op)
 
 
 if __name__ == '__main__':
