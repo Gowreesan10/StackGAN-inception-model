f98a4e184af25905ef0eb34856c3f841e6a6dac3
Sachchithananthan Gowreeshan <gowreeshan1@gmail.com>
2024-02-12 17:22:54 +0530

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -1,131 +1,108 @@
-"""A library to evaluate Inception on a single GPU.
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-import tensorflow as tf
-import math
-import os.path
-import sys
-if sys.version_info[0] == 2:
-    import cPickle as pickle
-else:
-    import pickle
-
 import os
+import math
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.applications import InceptionV3
 from tensorflow.keras.applications.inception_v3 import preprocess_input
-from tensorflow.keras.preprocessing import image
+from PIL import Image
 
+# Define flags
+FLAGS = tf.compat.v1.flags.FLAGS
+tf.compat.v1.flags.DEFINE_string('checkpoint_dir',
+                                 './inception_finetuned_models/birds_valid299/model.ckpt-5000',
+                                 """Path where to read model checkpoints.""")
+tf.compat.v1.flags.DEFINE_string('image_folder',
+                                 '/Users/han/Documents/CUB_200_2011/CUB_200_2011/images',
+                                 """Path where to load the images """)
+tf.compat.v1.flags.DEFINE_integer('num_classes', 50,      # 20 for flowers
+                                   """Number of classes """)
+tf.compat.v1.flags.DEFINE_integer('splits', 10,
+                                   """Number of splits """)
+tf.compat.v1.flags.DEFINE_integer('batch_size', 64, "batch size")
+tf.compat.v1.flags.DEFINE_integer('gpu', 1, "The ID of GPU to use")
 
-FLAGS = tf.app.flags.FLAGS
-
-tf.app.flags.DEFINE_string('checkpoint_dir',
-                           './inception_finetuned_models/birds_valid299/model.ckpt-5000',
-                           """Path where to read model checkpoints.""")
-
-tf.app.flags.DEFINE_string('image_folder', 
-							'/Users/han/Documents/CUB_200_2011/CUB_200_2011/images',
-							"""Path where to load the images """)
-
-tf.app.flags.DEFINE_integer('num_classes', 50,      # 20 for flowers
-                            """Number of classes """)
-tf.app.flags.DEFINE_integer('splits', 10,
-                            """Number of splits """)
-tf.app.flags.DEFINE_integer('batch_size', 64, "batch size")
-tf.app.flags.DEFINE_integer('gpu', 1, "The ID of GPU to use")
-# Batch normalization. Constant governing the exponential moving average of
-# the 'global' mean and variance for all activations.
+# Batch normalization constants
 BATCHNORM_MOVING_AVERAGE_DECAY = 0.9997
-
-# The decay to use for the moving average.
 MOVING_AVERAGE_DECAY = 0.9999
 
 
-fullpath = FLAGS.image_folder
-print(fullpath)
-
-def preprocess(img):
-    img = image.img_to_array(img)  # Assuming you have an image object
-    img = np.expand_dims(img, axis=0)
-    img = preprocess_input(img)  # InceptionV3 preprocessing
+def preprocess_image(img):
+    img = img.resize((299, 299))
+    img = np.array(img)
+    img = preprocess_input(img)
     return img
 
-def get_inception_score(sess, images, pred_op):
+
+def get_inception_score(images, pred_model):
     splits = FLAGS.splits
-    # assert(type(images) == list)
-    assert(type(images[0]) == np.ndarray)
+    assert(isinstance(images, list))
+    assert(isinstance(images[0], np.ndarray))
     assert(len(images[0].shape) == 3)
     assert(np.max(images[0]) > 10)
     assert(np.min(images[0]) >= 0.0)
+
     bs = FLAGS.batch_size
     preds = []
     num_examples = len(images)
     n_batches = int(math.floor(float(num_examples) / float(bs)))
     indices = list(np.arange(num_examples))
     np.random.shuffle(indices)
+
     for i in range(n_batches):
         inp = []
-        # print('i*bs', i*bs)
         for j in range(bs):
             if (i*bs + j) == num_examples:
                 break
             img = images[indices[i*bs + j]]
-            # print('*****', img.shape)
-            img = preprocess(img)
+            img = preprocess_image(img)
             inp.append(img)
-        # print("%d of %d batches" % (i, n_batches))
-        # inp = inps[(i * bs):min((i + 1) * bs, len(inps))]
         inp = np.concatenate(inp, 0)
-        #  print('inp', inp.shape)
-        pred = sess.run(pred_op, {'inputs:0': inp})
+        pred = pred_model.predict(inp)
         preds.append(pred)
-        # if i % 100 == 0:
-        #     print('Batch ', i)
-        #     print('inp', inp.shape, inp.max(), inp.min())
+
     preds = np.concatenate(preds, 0)
     scores = []
     for i in range(splits):
         istart = i * preds.shape[0] // splits
         iend = (i + 1) * preds.shape[0] // splits
         part = preds[istart:iend, :]
-        kl = (part * (np.log(part) -
-              np.log(np.expand_dims(np.mean(part, 0), 0))))
+        kl = (part * (np.log(part) - np.log(np.expand_dims(np.mean(part, 0), 0))))
         kl = np.mean(np.sum(kl, 1))
         scores.append(np.exp(kl))
     print('mean:', "%.2f" % np.mean(scores), 'std:', "%.2f" % np.std(scores))
     return np.mean(scores), np.std(scores)
 
 
-def load_data(fullpath):
+def load_images(fullpath):
+    print(fullpath)
     images = []
-    for path, subdirs, files in os.walk(fullpath):
+    for root, _, files in os.walk(fullpath):
         for name in files:
-            if name.rfind('jpg') != -1 or name.rfind('png') != -1:
-                filename = os.path.join(path, name)
-                if os.path.isfile(filename):
-                    img = image.load_img(filename)  # Using Keras for image loading
-                    images.append(img)
+            if name.endswith(('jpg', 'png')):
+                filename = os.path.join(root, name)
+                img = Image.open(filename)
+                images.append(img)
+    print('images', len(images), images[0].size)
     return images
 
+
 def main(unused_argv=None):
-    # Load InceptionV3 (exclude top for using our own output layer)
-    base_model = InceptionV3(
-        include_top=False, weights='imagenet', input_shape=(299, 299, 3)
-    )
+    config = tf.compat.v1.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
 
-    # Assuming you want logits directly for the defined number of classes
-    x = base_model.output
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    predictions = tf.keras.layers.Dense(FLAGS.num_classes, activation='softmax')(x)
-    model = tf.keras.Model(inputs=base_model.input, outputs=predictions)
+    # Load InceptionV3 model
+    inception_model = InceptionV3(weights='imagenet', include_top=True)
 
-    model.load_weights(FLAGS.checkpoint_dir)  # Assuming it's a Keras model checkpoint
+    # Load images
+    images = load_images(FLAGS.image_folder)
 
-    images = load_data(fullpath)
-    get_inception_score(images, model) 
+    # Preprocess images
+    inception_input = [preprocess_image(img) for img in images]
+
+    # Get Inception score
+    inception_score = get_inception_score(inception_input, inception_model)
+    print("Inception Score:", inception_score)
+
 
 if __name__ == '__main__':
-    main()
+    tf.compat.v1.app.run()
